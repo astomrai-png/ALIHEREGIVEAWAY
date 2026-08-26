79556dfeeb8013eec619ed41ce4940a3da13832e
astomrai-png <astomrai-png@users.noreply.github.com>
2026-08-26 07:21:18 +0000

    Initial upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: '8954512727:AAG_qnItvP-NQw9D-Y4ZTiOSh1piaeO3ACY', // Bot Token Here 
+    
+    S7: 'ALI HERE BUG', // Owner, Name
+    
+    adminId: '8763895360', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/teammysterybyali', // TG channel link
+    
+    group: 'https://t.me/banproofsbyali', // TG group link
+    
+    bot: 'ALI HERE BUG', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
