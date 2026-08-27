46bd895a0ec4eda681272117d04650d6a5030f07
astomrai-png <astomrai-png@users.noreply.github.com>
2026-08-27 07:36:57 +0000

    Initial upload of rowdybug project

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    mainToken: '8854562696:AAEnEh02DNBSMU1QjL0ZTFbzh7Hs2PQtSUs', // Bot Token Here 
+    
+    S7: 'ROWDY HERE BUG', // Owner, Name
+    
+    adminId: '8691305020, // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/astomrai123', // TG channel link
+    
+    group: 'https://t.me/rowdyboy123456', // TG group link
+    
+    bot: 'ROWDY HERE BUG', // Bot name 
+    
+    logo: './ROWDY/Loves.jpg' // Bot logo path or URL
+};
